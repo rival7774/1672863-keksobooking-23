20545011665b25389b2c460375ad76e30be3f13f
Samir <sakitov@yandex.ru>
2021-06-01 00:34:49 +0400

    Сделал два варианта функций

--- js/main.js
+++ js/main.js
@@ -1,64 +1,48 @@
-//!! Ссылка на источник генерации https://myrusakov.ru/js-random-numbers.html
-// const getRandomInRange = (min, max) => {
-//   if (!(min >= max) && min >= 0) {
-//     return Math.floor(Math.random() * (max - min + 1)) + min;
-//   } else if (min !== max && !(max < 0) && !(min < 0) && min > max) {
-//     return Math.abs(Math.floor(Math.random() * (min - max + 1)) + max);
-//   } else if ((max < 0 || min < 0) && Math.abs(max) > Math.abs(min)) {
-//     return Math.random() * (Math.abs(max) - Math.abs(min) + 1) + Math.abs(min);
-//   } else if ((max < 0 || min < 0) && Math.abs(max) < Math.abs(min)) {
-//     return Math.random() * (Math.abs(min) - Math.abs(max) + 1) + Math.abs(max);
-//   } else if (min === max) {
-//     return min;
-//   }
-// };
-
+//!!*Ссылка на источник генерации https://myrusakov.ru/js-random-numbers.html
 const getRandomInRange = (min, max, number) => {
-  if (!(Math.abs(min) >= Math.abs(max))) {
-    return Math.random() * (Math.abs(max) - Math.abs(min) + 1) + Math.abs(min);
-  } else if (min !== max && Math.abs(min) > Math.abs(max)) {
-    return Math.random() * (Math.abs(min) - Math.abs(max) + 1) + Math.abs(max);
-  } else if (min === max) {
-    return min;
-  }
-};
-
-getRandomInRange(0, 1);
-//!! https://coderoad.ru/29101892/%D0%9A%D0%B0%D0%BA-%D1%81%D0%B4%D0%B5%D0%BB%D0%B0%D1%82%D1%8C-%D1%82%D0%BE%D1%87%D0%BD%D0%BE%D1%81%D1%82%D1%8C-%D1%87%D0%B8%D1%81%D0%B5%D0%BB-%D1%81-%D0%BF%D0%BB%D0%B0%D0%B2%D0%B0%D1%8E%D1%89%D0%B5%D0%B9-%D0%B7%D0%B0%D0%BF%D1%8F%D1%82%D0%BE%D0%B9-%D0%BE%D0%B4%D0%B8%D0%BD%D0%B0%D0%BA%D0%BE%D0%B2%D0%BE%D0%B9-%D1%81-%D0%BF%D0%BE%D0%BC%D0%BE%D1%89%D1%8C%D1%8E-Javascript
-// const getRandomInRange1 = (min, max, number) => {
-//   let theOutcome;
-//   if (!(min >= max) && min >= 0) {
-//     theOutcome = Math.random() * (max - min + 1) + min;
-//   } else if (min !== max && !(max < 0) && !(min < 0) && min > max) {
-//     theOutcome = Math.random() * (min - max + 1) + max;
-//   } else if ((max < 0 || min < 0) && Math.abs(max) > Math.abs(min)) {
-//     theOutcome =
-//       Math.random() * (Math.abs(max) - Math.abs(min) + 1) + Math.abs(min);
-//   } else if ((max < 0 || min < 0) && Math.abs(max) < Math.abs(min)) {
-//     theOutcome =
-//       Math.random() * (Math.abs(min) - Math.abs(max) + 1) + Math.abs(max);
-//   } else if (min === max) {
-//     theOutcome = min;
-//   }
-//   return theOutcome.toFixed(number);
-// };
-
-// getRandomInRange1(0, 10, 5);
-// console.log(getRandomInRange1(0, -10, 5));
-
-const getRandomInRange1 = (min, max, number) => {
+  let absMin = Math.abs(min);
+  let absMax = Math.abs(max);
   let theOutcome;
-  if (!(Math.abs(min) >= Math.abs(max))) {
-    theOutcome =
-      Math.random() * (Math.abs(max) - Math.abs(min) + 1) + Math.abs(min);
-  } else if (min !== max && Math.abs(min) > Math.abs(max)) {
-    theOutcome =
-      Math.random() * (Math.abs(min) - Math.abs(max) + 1) + Math.abs(max);
-  } else if (min === max) {
-    theOutcome = min;
+
+  if (absMin > absMax) {
+    const temp = absMin;
+    absMin = absMax;
+    absMax = temp;
   }
+
+  if (!number) {
+    theOutcome = Math.floor(Math.random() * (absMax - absMin + 1) + absMin);
+    return theOutcome;
+  }
+
+  theOutcome = Math.random() * (absMax - absMin + 1) + absMin;
   return theOutcome.toFixed(number);
 };
 
-getRandomInRange1(0, 10, 5);
+console.log(getRandomInRange(1, -10, 4));
+
+//!! ************************************************************************************
+
+const getRandomInRange1 = (min, max) => {
+  let absMin = Math.abs(min);
+  let absMax = Math.abs(max);
+
+  if (absMin > absMax) {
+    const temp = absMin;
+    absMin = absMax;
+    absMax = temp;
+  }
+
+  return Math.floor(Math.random() * (absMax - absMin + 1) + absMin);
+};
+
+console.log(getRandomInRange1(1, -10));
+
+const getRandomInRange2 = (min, max, number) => {
+  const theOutcome = getRandomInRange(min, max) + Math.random();
+
+  return theOutcome.toFixed(number);
+};
+
+getRandomInRange2(0, 10, 5);
 console.log(getRandomInRange1(-5, -10, 5));
