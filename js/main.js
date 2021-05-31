49111fba41bd6f2780cdbb9c1b81cd43312b308f
Samir <sakitov@yandex.ru>
2021-06-01 01:12:44 +0400

    Убрал консоли

--- js/main.js
+++ js/main.js
@@ -19,8 +19,6 @@ const getRandomInRange = (min, max, number) => {
   return theOutcome.toFixed(number);
 };
 
-console.log(getRandomInRange(1, -10, 4));
-
 //!! ************************************************************************************
 
 const getRandomInRange1 = (min, max) => {
@@ -36,8 +34,6 @@ const getRandomInRange1 = (min, max) => {
   return Math.floor(Math.random() * (absMax - absMin + 1) + absMin);
 };
 
-console.log(getRandomInRange1(1, -10));
-
 const getRandomInRange2 = (min, max, number) => {
   const theOutcome = getRandomInRange(min, max) + Math.random();
 
@@ -45,4 +41,3 @@ const getRandomInRange2 = (min, max, number) => {
 };
 
 getRandomInRange2(0, 10, 5);
-console.log(getRandomInRange1(-5, -10, 5));
