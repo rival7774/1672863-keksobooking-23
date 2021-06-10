0a7e36b176241539413119894cc9a6a142a8a2f0
Samir <sakitov@yandex.ru>
2021-06-11 00:50:24 +0400

    Сделал 10 объектов

--- js/main.js
+++ js/main.js
@@ -38,34 +38,66 @@ getRandomInRange(-100, -10, 2);
 
 const typeOfHousing = ['palace', 'flat', 'house', 'bungalow', 'hotel'];
 const time = ['12:00', '13:00', '14:00'];
+const comfort = ['wifi', 'dishwasher', 'parking', 'washer', 'elevator', 'conditioner'];
+const foto = ['https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/duonguyen-8LrGtIxxa4w.jpg', 'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/brandon-hoogenboom-SNxQGWxZQi0.jpg', 'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/claire-rendall-b6kAwr1i0Iw.jpg'];
+const randomAvatar = `img/avatars/user0${  getRandomInRange(1, 8)  }.png`;
 
-const author = {
-  avatar: 'img/avatars/user0' + getRandomInRange(1, 8) + '.png',
+const getRandomLengthArray = (arr) => {
+  const array = Array.from({length: getRandomInRange(1, arr.length)});
+
+  let index = 0;
+  while (index < array.length) {
+    const randomValue = arr[getRandomInRange(0, arr.length)];
+
+    if (!array.includes(randomValue)) {
+      array[index] = randomValue;
+      index++;
+    }
+  }
+  return array;
 };
 
-const offer = {
-  title: 'Шикарные апортаменты',
-  address: 'location.53.5303000° location.49.3461000°',
-  price: getRandomInRange(1000, 10000),
-  type: typeOfHousing[getRandomInRange(0, typeOfHousing.length - 1)],
-  rooms: getRandomInRange(1, 6),
-  guests: getRandomInRange(1, 20),
-  checkin: time[getRandomInRange(0, time.length - 1)],
-  checkout: time[getRandomInRange(0, time.length - 1)],
-  features: [
-    'wifi',
-    'dishwasher',
-    'parking',
-    'washer',
-    'elevator',
-    'conditioner',
-  ],
-  description: 'описание помещения',
-  photos: [
-    'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/duonguyen-8LrGtIxxa4w.jpg',
-    'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/brandon-hoogenboom-SNxQGWxZQi0.jpg',
-    'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/claire-rendall-b6kAwr1i0Iw.jpg',
-  ],
+const getRandomAds = () => {
+  const object = {
+    author: {
+      avatar: randomAvatar,
+    },
+    offer: {
+      title: 'Шикарные апортаменты',
+      address: 'location.53.5303000° location.49.3461000°',
+      price: getRandomInRange(1000, 10000),
+      type: typeOfHousing[getRandomInRange(0, typeOfHousing.length - 1)],
+      rooms: getRandomInRange(1, 6),
+      guests: getRandomInRange(1, 20),
+      checkin: time[getRandomInRange(0, time.length - 1)],
+      checkout: time[getRandomInRange(0, time.length - 1)],
+      features: getRandomLengthArray(comfort),
+      description: 'описание помещения',
+      photos: getRandomLengthArray(foto),
+    },
+    location: {
+      lat: getRandomInRange(35.65000, 35.70000, 5),
+      lng: getRandomInRange(139.70000, 139.80000, 5),
+    },
+  };
+  return object;
 };
 
-console.log(offer);
+const NUMBER_OF_ADS = 10;
+
+const getArrayOfDeclarations = (numberOfAds) => {
+  // const array = Array.from({length: numberOfAds}).fill(1);
+
+  // for (let index = 0; index < numberOfAds; index++) {
+  //   array[index] = getRandomAds();
+  // }
+
+  // array.forEach((element, index) => {
+  //   array[index] = getRandomAds();
+  // });
+  // return array;
+
+  return new Array(numberOfAds).fill('1').map(() => getRandomAds());
+};
+
+console.log(getArrayOfDeclarations(NUMBER_OF_ADS));
